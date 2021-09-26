f399da6933ae3480509ae336f84fe940bea01b26
Heikki-Jussi Niemi <hekku2@gmail.com>
2021-09-26 21:03:54 +0300

    Add quick and dirty reload for series

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Subscription, timer } from 'rxjs';
 import { SeriesResultDto } from './shared/series-result-dto';
+import { FilelistDto } from './shared/filelist-dto';
 
 @Component({
   selector: 'app-root',
@@ -13,76 +15,62 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'event-scoreboard';
 
   private seriesChangeTime = 5000;
+  private backgroundUpdateTime = 10000;
 
   selectedIndex: number = 0;
   selectedSeries: SeriesResultDto | undefined;
 
-  mockResults: SeriesResultDto[] = [
-    {
-      seriesName: 'Senior Women',
-      results: [
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija',
-        '1. Meikäläinen, Maija'
-      ]
-    } as SeriesResultDto,
-    {
-      seriesName: 'Senior Men',
-      results: [
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti',
-        '1. Meikäläinen, Matti'
-      ]
-    } as SeriesResultDto,
-    {
-      seriesName: 'Junior A',
-      results: [
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna',
-        '1. Meikäläinen, Minna'
-      ]
-    } as SeriesResultDto,
-    {
-      seriesName: 'Junior B',
-      results: [
-        '1. Meikäläinen, Seija',
-        '1. Meikäläinen, Seija',
-        '1. Meikäläinen, Seija',
-      ]
-    } as SeriesResultDto,
-  ]
+  loadedResults: SeriesResultDto[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.subscription.add(timer(0, this.seriesChangeTime).subscribe(n => this.advanceSeries()));
+    this.subscription.add(timer(0, this.backgroundUpdateTime).subscribe(n => this.updateResults()));
+    this.selectedSeries = undefined;
 
-    this.selectedSeries = this.mockResults[this.selectedIndex];
+    this.updateResults();
   }
 
   public ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
+  updateResults() {
+    console.log('Updating results.');
+    this.http.get('../assets/results/filelist.json').subscribe(
+      data => {
+        var filelistDto = data as FilelistDto;
+        var files = filelistDto.files;
+        console.log('Filelist fetched.');
+
+        files.forEach(filename => {
+          this.http.get(`../assets/results/${filename}`).subscribe(result => {
+            console.log(result);
+            var resultDto = result as SeriesResultDto;
+            console.log(`Updating ${resultDto.seriesName}`);
+
+            const match = this.loadedResults.find(s => s.seriesName === resultDto.seriesName)
+            if (match) {
+              match.results = resultDto.results;
+            } else {
+              this.loadedResults.push(resultDto);
+            }
+          });
+        });
+      }
+    );
+  }
+
   advanceSeries() {
-    this.selectedIndex = (this.selectedIndex + 1) % this.mockResults.length;
-    this.selectedSeries = this.mockResults[this.selectedIndex];
+    console.log('Advancing');
+    if (this.loadedResults.length === 0) {
+      console.log('No results found.');
+      this.selectedSeries = undefined;
+      return;
+    }
+    this.selectedIndex = (this.selectedIndex + 1) % this.loadedResults.length;
+    this.selectedSeries = this.loadedResults[this.selectedIndex];
   }
 
 }
---
+++ site/src/app/shared/filelist-dto.ts
@@ -0,0 +1,3 @@
+export interface FilelistDto {
+    files: string[];
+}
